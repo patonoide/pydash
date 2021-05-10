ecb717b8138fa7c6ced6eeaf36c0501790964f70
Gabriel <patonoideoriginal@gmail.com>
2021-05-10 16:29:20 -0300

    feature: adiciona otimização

--- r2a/r2aadaptive.py
+++ r2a/r2aadaptive.py
@@ -5,6 +5,7 @@ from player.parser import *
 from r2a.ir2a import IR2A
 import time
 import math
+from base.whiteboard import Whiteboard
 
 
 class R2AAdaptive(IR2A):
@@ -17,9 +18,10 @@ class R2AAdaptive(IR2A):
         self.real_throughput_list = []
         self.estimated_throughput_list = []
         self.constraint_list = []
-        self.mu = 0.25
+        self.mu = 0.2
         self.k = 21
-        self.p0 = 0.3
+        self.p0 = 0.25
+        self.whiteboard = Whiteboard.get_instance()
 
     def handle_xml_request(self, msg):
         self.request_time = time.perf_counter()
@@ -47,7 +49,6 @@ class R2AAdaptive(IR2A):
         msg.add_quality_id(self.calculate_quality_id())
         self.send_down(msg)
 
-
     def handle_segment_size_response(self, msg):
         t = time.perf_counter() - self.request_time
         self.real_throughput_list.append(msg.get_bit_length() / t)
@@ -60,8 +61,8 @@ class R2AAdaptive(IR2A):
         pass
 
     def calculate_delta(self):
-        denominador = 1 + math.exp(-self.k*(self.calculate_p()-self.p0))
-        return (1/denominador)
+        denominador = 1 + math.exp(-self.k * (self.calculate_p() - self.p0))
+        return 1 / denominador
 
     def calculate_estimated_throughput(self):
         if len(self.estimated_throughput_list) <= 2:
@@ -70,22 +71,32 @@ class R2AAdaptive(IR2A):
             delta = self.calculate_delta()
             te2 = self.estimated_throughput_list[-2]
             ts1 = self.real_throughput_list[-1]
-            te = (1 - delta)*te2 + delta*ts1
+            te = (1 - delta) * te2 + delta * ts1
             self.estimated_throughput_list.append(te)
 
-
     def calculate_constraint(self):
-        constraint = (1 - self.mu)*self.estimated_throughput_list[-1]
+        constraint = (1 - self.mu) * self.estimated_throughput_list[-1]
         self.constraint_list.append(constraint)
 
     def calculate_p(self):
-        return (abs(self.real_throughput_list[-1] - self.estimated_throughput_list[-1])/self.estimated_throughput_list[-1])
+        return (abs(self.real_throughput_list[-1] - self.estimated_throughput_list[-1]) /
+                self.estimated_throughput_list[-1])
 
     def calculate_quality_id(self):
         last_rc = self.constraint_list[-1]
 
+        if len(self.whiteboard.get_playback_buffer_size()) == 0:
+            return self.qi_list[0]
+
+        if self.whiteboard.get_playback_buffer_size()[-1][1] >= 20:
+            self.mu = 0.10
+            self.k = 23
+        else:
+            self.mu = 0.2
+            self.k = 21
+
         best_qi = self.qi_list[0]
-        for qi in (self.qi_list):
-            if(qi > best_qi and qi <= last_rc):
+        for qi in self.qi_list:
+            if best_qi < qi <= last_rc:
                 best_qi = qi
         return best_qi
